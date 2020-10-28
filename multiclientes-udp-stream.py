25b400a7d3098922653b4014bd1f4c02789b1c0f
Julian <je.jaimes@uniandes.edu.co>
2020-10-27 21:27:39 -0500

    Multiple clients wont work

--- multiclientes-udp-stream.py
+++ multiclientes-udp-stream.py
@@ -9,10 +9,10 @@ import threading
 
 clicked = False
 
-def onMouse(event, x, y, flags, param):
-    global clicked
-    if event == cv2.EVENT_LBUTTONUP:
-        clicked = True
+#def onMouse(event, x, y, flags, param):
+#    global clicked
+#    if event == cv2.EVENT_LBUTTONUP:
+#        clicked = True
 
 def ordenarbytes(h):
     r = b''
